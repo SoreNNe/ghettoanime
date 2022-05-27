20a0cf13cfb1d9c122a4b09841cd142086530ef8
SoreNNe <71930836+SoreNNe@users.noreply.github.com>
2022-05-27 16:48:15 +0200

    waiting for database

--- ghettoanime-react/src/components/Main.js
+++ ghettoanime-react/src/components/Main.js
@@ -1,9 +1,26 @@
 import React from "react";
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
 
 export default function Main(props){
+    const [cards, setCards] = useState([]);
+    useEffect(() => {
+     const fetchData = async () => {
+        const result = await fetch("http://localhost:8080/api/v1/series", {
+          crossDomain: true,
+        });
+        const json = await result.json();
+        //console.log(json);
+        setCards(json);
+      };
+      fetchData();
+    },[cards]);
     return(
+        
         <main>
+            {cards.map((card) => (
+            <Main {...card} />
+          ))}
             <div className="card">
             <Link to="/WatchingPage"><img src={props.url} className="card--image"/></Link>
                 <div className="card--stats">
